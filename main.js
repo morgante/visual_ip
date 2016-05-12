0d9ca5ccced3ac72af39573d62b48200a4ff16c1
Morgante Pell <morgante.pell@morgante.net>
2016-05-12 13:22:24 +0400

    Debouncing, etc.

--- main.js
+++ main.js
@@ -24,7 +24,7 @@ const mySource = 2;
 function createWindow () {
   // Create the browser window.
   mainWindow = new BrowserWindow({
-    fullscreen: true
+    // fullscreen: true
   });
 
   // and load the index.html of the app.
@@ -47,60 +47,66 @@ function createWindow () {
       console.log(port.pnpId);
       console.log(port.manufacturer);
     });
-  });
 
-  var port = new SerialPort('/dev/cu.usbmodem1421', {
-    baudrate: 9600,
-    parser: serialport.parsers.readline('\n')
-  });
+    var port = new SerialPort(ports[3].comName, {
+      baudrate: 115200,
+      parser: serialport.parsers.readline('\n')
+    });
 
-  port.on('open', function () {
-    console.log('opened serial port');
-  });
+    port.on('open', function () {
+      console.log('opened serial port');
+    });
 
-  port.on('data', function (data) {
-    if (data === lastCard) {
-      return;
-    } else {
-      lastCard = data;
-    }
-    const id = parseInt(data, 10);
-    if (_.isNaN(id)) {
-      console.log('data is not a card', data);
-    } else {
-      console.log('load the card', id);
-      var dataRef = new Firebase(`https://vipneteverything.firebaseio.com/data/packets/${id}`);
-      if (!lastEvent) {
+    const clearCard = _.debounce(() => {
+      console.log("clear card");
+      lastCard = null;
+    }, 5000);
+
+    port.on('data', function (data) {
+      if (data === lastCard) {
         return;
+      } else {
+        lastCard = data;
+        clearCard();
       }
-      dataRef.child("message").once("value", function(snapshot) {
-        console.log('snapshot', snapshot.val());
-        if (snapshot.val() !== null && snapshot.val().source !== mySource && snapshot.val().text) {
-          console.log('some other data');
-          lastEvent.sender.send('receive_data', snapshot.val());
-        } else {
-          if (dataMessage.length < 1) {
-            if (lastEvent) {
-              lastEvent.sender.send('alert', 'Please type a message before attempting to send it!');
-            }
-            return;
-          } else {
-            dataRef.set({
-              message: {
-                source: mySource,
-                text: dataMessage
-              }
-            });
-            lastEvent.sender.send('sent_input', 'Sent');
-          }
+      const id = parseInt(data, 10);
+      if (_.isNaN(id)) {
+        console.log('data is not a card', data);
+      } else {
+        console.log('load the card', id);
+        var dataRef = new Firebase(`https://vipneteverything.firebaseio.com/data/packets/${id}`);
+        if (!lastEvent) {
+          return;
         }
-      });
-    }
-  });
+        dataRef.child("message").once("value", function(snapshot) {
+          console.log('snapshot', snapshot.val());
+          if (snapshot.val() !== null && snapshot.val().source !== mySource && snapshot.val().text) {
+            console.log('some other data');
+            lastEvent.sender.send('receive_data', snapshot.val());
+          } else {
+            if (dataMessage.length < 1) {
+              if (lastEvent) {
+                lastEvent.sender.send('alert', 'Please type a message before attempting to send it!');
+              }
+              return;
+            } else {
+              dataRef.set({
+                message: {
+                  source: mySource,
+                  text: dataMessage
+                }
+              });
+              lastEvent.sender.send('sent_input', 'Sent');
+            }
+          }
+        });
+      }
+    });
 
-  ipcMain.on('set_input', function(event, arg) {
-    dataMessage = arg;
-    lastEvent = event;
+    ipcMain.on('set_input', function(event, arg) {
+      dataMessage = arg;
+      lastEvent = event;
+    });
   });
 }
 
